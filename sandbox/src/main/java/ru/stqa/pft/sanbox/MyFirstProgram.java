15d67681a7970d96c6e840003f691be9d29171b7
SYProkopchuk <prokopchuk.serg@mail.ru>
2019-06-01 20:56:53 +0400

    Added methods

--- sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirstProgram.java
@@ -1,7 +1,27 @@
 package ru.stqa.pft.sanbox;
 
 public class MyFirstProgram {
+
 public static void main (String[] args) {
-	System.out.println("Hello, world!");
-	}
+	hello("hello");
+	hello("misha!");
+	hello("eto");
+	hello("ti?");
+	double len=5;
+	double d=6;
+	System.out.println(area(len,d));
+
+}
+
+public static void hello(String something){
+	System.out.println("Hello, " + something);
+}
+public static double area (double jopa) {
+
+	return jopa*jopa;
+}
+
+public static double area(double a, double b) {
+	return a*b;
+}
 }
